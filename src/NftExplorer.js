51914fe25cb46ee91be8b0c55671716a2c0a2613
vboxuser <vboxuser@blockspan-ubuntu.myguest.virtualbox.org>
2023-06-21 15:09:07 -0600

    API call working once

--- src/NftExplorer.js
+++ src/NftExplorer.js
@@ -1,38 +1,50 @@
 import React, { useState } from 'react';
-import nftService from './services/nftService';
+// import nftService from './services/nftService';
+import axios from 'axios'
 
 const NftExplorer = () => {
   const [metadata, setMetadata] = useState(null);
   const [contractAddress, setContractAddress] = useState('');
   const [tokenId, setTokenId] = useState('');
+  // const [blockchain, setBlockchain] = useState('eth-main');
   const [ownerAddress, setOwnerAddress] = useState('');
+  const [error, setError] = useState(null);
 
   const fetchNftMetadata = async () => {
+    const options = {
+      method: 'PUT',
+      url: `http://localhost:8080/v1/refresh/contract/0xce4f341622340d56e397740d325fd357e62b91cb/token/1773`,
+      headers: { accept: 'application/json', 'X-API-KEY': '2jhzbqIWanB8puiqySBIWJVf6Ovp7oPW' }
+    };
+  
     try {
-      const response = await nftService.getNftMetadata(contractAddress, tokenId);
-      setMetadata(response.data);
+      const response = await axios.request(options);
+      console.log('response', response);
+      setMetadata(response.data.price_summary);
+      setError(null);
     } catch (error) {
-      console.error('Error fetching NFT metadata:', error);
+      console.log('error')
+      setError('Error: Verify chain and contract address are valid!');
     }
   };
 
- const fetchCollectionMetadata = async () => {
-    try {
-      const response = await nftService.getCollectionMetadata(contractAddress);
-      setMetadata(response.data);
-    } catch (error) {
-      console.error('Error fetching collection metadata:', error);
-    }
-  };
+//  const fetchCollectionMetadata = async () => {
+//     try {
+//       const response = await nftService.getCollectionMetadata(contractAddress);
+//       setMetadata(response.data);
+//     } catch (error) {
+//       console.error('Error fetching collection metadata:', error);
+//     }
+//   };
 
-  const fetchOwnerMetadata = async () => {
-    try {
-      const response = await nftService.getOwnerMetadata(ownerAddress);
-      setMetadata(response.data);
-    } catch (error) {
-      console.error('Error fetching owner metadata:', error);
-    }
-  };
+//   const fetchOwnerMetadata = async () => {
+//     try {
+//       const response = await nftService.getOwnerMetadata(ownerAddress);
+//       setMetadata(response.data);
+//     } catch (error) {
+//       console.error('Error fetching owner metadata:', error);
+//     }
+//   };
 
   return (
     <div>
@@ -55,8 +67,8 @@ const NftExplorer = () => {
         placeholder="Owner address"
       />
       <button onClick={fetchNftMetadata}>Fetch NFT Metadata</button>
-      <button onClick={fetchCollectionMetadata}>Fetch Collection Metadata</button>
-      <button onClick={fetchOwnerMetadata}>Fetch Owner Metadata</button>
+      {/* <button onClick={fetchCollectionMetadata}>Fetch Collection Metadata</button>
+      <button onClick={fetchOwnerMetadata}>Fetch Owner Metadata</button> */}
       <pre>{metadata ? JSON.stringify(metadata, null, 2) : 'Loading...'}</pre>
     </div>
   );
